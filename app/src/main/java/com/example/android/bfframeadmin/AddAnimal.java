3f81c2d12523a4ea815ded5c0625f0e7e6ffbf3f
Diasfu <diasfu@gmail.com>
2017-03-01 14:41:40 +0000

    Almost finished frames

---
+++ app/src/main/java/com/example/android/bfframeadmin/AddAnimal.java
@@ -0,0 +1,27 @@
+package com.example.android.bfframeadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.support.v7.appcompat.R.styleable.View;
+
+public class AddAnimal extends AppCompatActivity {
+
+    private EditText mNameOfAnimal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_animal);
+
+        mNameOfAnimal = (EditText)findViewById(R.id.id_name_of_animal);
+    }
+
+    public void onClickSaveButton(View view) {
+        String animalName = mNameOfAnimal.getText().toString();
+        Toast.makeText(this, animalName + " was stored!", Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/example/android/bfframeadmin/OptionsInitialPage.java
+++ app/src/main/java/com/example/android/bfframeadmin/OptionsInitialPage.java
@@ -1,8 +1,21 @@
 package com.example.android.bfframeadmin;
 
-/**
- * Created by diasf on 01/03/2017.
- */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
-public class OptionsInitialPage {
+import static com.example.android.bfframeadmin.R.styleable.View;
+
+public class OptionsInitialPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options_initial_page);
+    }
+    public void onClickAddAnimal(View view){
+        Intent intent = new Intent(this, AddAnimal.class);
+        startActivity(intent);
+    }
 }
